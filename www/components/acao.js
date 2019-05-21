0712e7a411ae1a44d25e856e1ef6319a93e4afef
vlvkvinicius <lourenco_vinicius@yahoo.com>
2019-05-22 04:11:47 +0900

    front

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,21 @@
 // This is a JavaScript file
 
+$(document).on("click","#cadastrar",function(){
+     var parametros = {
+          "nome": $("#nome").val(),
+          "email": $("#email").val()
+     };
+     $.ajax({
+          type:"post",
+          url:"https://mobile2ds2-vlvkvinicius.c9users.io/webservice/cadastro.php",
+          data:parametros,
+          success:function(data){
+               navigator.notification.alert(data);
+               $("#nome").val("");
+               $("#email").val("");
+          },
+          error: function(data){
+               navigator.notification.alert(data);
+          }
+     });
+});
